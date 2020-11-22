bc8d140fdbeb40b9e34975cca98cbf84126b18f1
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2020-11-22 12:20:55 +0500

    Update

--- lib/src/ui/widgets/chat_container.dart
+++ lib/src/ui/widgets/chat_container.dart
@@ -30,7 +30,9 @@ class ChatContainer extends StatelessWidget {
             );
           } else if (state is ChatMoving) {
             size = state.size;
-          } else if (state is ChatCollapsed) {}
+          } else if (state is ChatCollapsed) {
+            return getCollapsedChat();
+          }
           return Align(
             alignment: Alignment.bottomCenter,
             child: AnimatedContainer(
@@ -44,7 +46,7 @@ class ChatContainer extends StatelessWidget {
                   topRight: Radius.circular(24.0),
                 ),
               ),
-              child: getColumn(context),
+              child: getWidgets(context),
             ),
           );
         },
@@ -52,9 +54,19 @@ class ChatContainer extends StatelessWidget {
     );
   }
 
-  Widget getColumn(BuildContext context) {
+  Widget getWidgets(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        getChatHeader(),
+        getChatSpace(),
+        getChatControl(context),
+      ],
+    );
+  }
+
+  Widget getChatHeader({bool collapsed = false}) {
+    return Column(
       children: [
         Container(
           height: 15,
@@ -76,14 +88,16 @@ class ChatContainer extends StatelessWidget {
           child: AppBar(
             backgroundColor: Colors.grey.withOpacity(0.2),
             elevation: 0,
-            leading: FlatButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Icon(
-                  Icons.arrow_back_ios,
-                  color: kRedColor,
-                )),
+            leading: collapsed
+                ? Container()
+                : FlatButton(
+                    onPressed: () {
+                      // TODO: Add functionality
+                    },
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      color: kRedColor,
+                    )),
             title: Row(
               children: <Widget>[
                 Container(
@@ -136,203 +150,106 @@ class ChatContainer extends StatelessWidget {
                 size: 35,
               ),
               SizedBox(
-                width: 8,
-              ),
-              Container(
-                width: 13,
-                height: 13,
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Colors.white38)),
-              ),
-              SizedBox(
                 width: 15,
               ),
             ],
-          ),
-        ),
-        Expanded(
-          child: Container(),
-        ),
-        Container(
-          height: 60,
-          width: double.infinity,
-          decoration: BoxDecoration(color: Colors.grey.withOpacity(0.4)),
-          child: Padding(
-            padding: EdgeInsets.only(left: 15, right: 15, bottom: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Expanded(
-                  flex: 2,
-                  //width: (MediaQuery.of(context).size.width - 60) / 2,
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.add_circle,
-                        size: 30,
-                        color: kRedColor,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Icon(
-                        Icons.camera_alt,
-                        size: 30,
-                        color: kRedColor,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Icon(
-                        Icons.photo,
-                        size: 30,
-                        color: kRedColor,
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  flex: 4,
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(top: 2.0),
-                        child: Container(
-                          width: (MediaQuery.of(context).size.width - 10) / 2,
-                          height: 40,
-                          decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(20)),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 12),
-                            child: TextField(
-                              cursorColor: Colors.black,
-                              //controller: _sendMessageController,
-                              decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'Text Message',
-                                  suffixIcon: Icon(
-                                    Icons.face,
-                                    color: kRedColor,
-                                    size: 28,
-                                  )),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Icon(
-                        Icons.thumb_up,
-                        size: 30,
-                        color: kRedColor,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
           ),
         ),
       ],
     );
   }
 
-  Widget getCollapsedChat() {
-    return Column(
-      children: [
-        Container(
-          height: 15,
-          color: Colors.grey.withOpacity(0.2),
-          child: Padding(
-            padding: const EdgeInsets.only(
-              top: 12.0,
-              left: 135,
-              right: 135,
-            ),
-            child: Divider(
-              color: Colors.black54,
-              thickness: 5,
-            ),
-          ),
-        ),
-        PreferredSize(
-          preferredSize: Size.fromHeight(20),
-          child: AppBar(
-            backgroundColor: Colors.grey.withOpacity(0.2),
-            elevation: 0,
-            title: Row(
-              children: <Widget>[
-                Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"),
-                          fit: BoxFit.cover)),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'The\tCabinet',
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Text(
-                      'Talha, Ali, Osama',
-                      style: TextStyle(
-                          color: Colors.black.withOpacity(0.4), fontSize: 14),
-                    )
-                  ],
-                )
-              ],
-            ),
-            actions: <Widget>[
-              Icon(
-                Icons.phone,
-                color: kRedColor,
-                size: 32,
-              ),
-              SizedBox(
-                width: 15,
-              ),
-              Icon(
-                Icons.videocam_sharp,
-                color: kRedColor,
-                size: 35,
-              ),
-              SizedBox(
-                width: 8,
-              ),
-              Container(
-                width: 13,
-                height: 13,
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Colors.white38)),
-              ),
-              SizedBox(
-                width: 15,
-              ),
-            ],
-          ),
-        ),
-      ],
+  Widget getChatSpace() {
+    return Expanded(
+      child: Container(),
     );
+  }
+
+  Widget getChatControl(BuildContext context) {
+    return Container(
+      height: 60,
+      width: double.infinity,
+      decoration: BoxDecoration(color: Colors.grey.withOpacity(0.4)),
+      child: Padding(
+        padding: EdgeInsets.only(left: 15, right: 15, bottom: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.add_circle,
+                    size: 30,
+                    color: kRedColor,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    Icons.camera_alt,
+                    size: 30,
+                    color: kRedColor,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    Icons.photo,
+                    size: 30,
+                    color: kRedColor,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 2.0),
+                    child: Container(
+                      width: (MediaQuery.of(context).size.width - 10) / 2,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 12),
+                        child: TextField(
+                          cursorColor: Colors.black,
+                          //controller: _sendMessageController,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Text Message',
+                              suffixIcon: Icon(
+                                Icons.face,
+                                color: kRedColor,
+                                size: 28,
+                              )),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Icon(
+                    Icons.thumb_up,
+                    size: 30,
+                    color: kRedColor,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getCollapsedChat() {
+    return getChatHeader(collapsed: true);
   }
 }
