f405272a173609ff389431838bb88d6efc3a204d
ihassanjavaid <great.maliks@gmail.com>
2020-11-22 19:32:45 +0500

    update

--- lib/src/ui/main_screen.dart
+++ lib/src/ui/main_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:the_chat_app/src/resources/utilities/constants.dart';
 import 'package:the_chat_app/src/ui/widgets/chat_container.dart';
-import 'package:intl/intl.dart';
 
 class MainScreen extends StatelessWidget {
   static const String id = 'main_screen';
@@ -14,15 +14,18 @@ class MainScreen extends StatelessWidget {
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xffc0012a),
-                Color(0xffd94e23),
-                Color(0xffe78c70),
-              ],
-              stops: [0.1, 0.4, 0.7]
-            ),
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xffc0012a),
+                  Color(0xffd94e23),
+                  Color(0xffe78c70),
+                ],
+                stops: [
+                  0.1,
+                  0.4,
+                  0.7
+                ]),
           ),
           child: getColumn(context),
         ),
@@ -58,65 +61,31 @@ class MainScreen extends StatelessWidget {
             alignment: constraints.biggest.height >= threshold
                 ? Alignment.center
                 : Alignment.centerLeft,
-            child: /*Text(
-              'THISRTY THURSDAY'
-                  '${getCurretTime()}',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'CM Sans Serif',
-                  fontSize: 24.0,
-                  //fontWeight: FontWeight.bold
-              ),
-            ),*/
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'THIRSTY THURSDAY',
-                    style: TextStyle(
-                      color: Colors.white,
+            child: RichText(
+              text: TextSpan(children: [
+                TextSpan(
+                  text: 'THIRSTY THURSDAY',
+                  style: TextStyle(
+                      color: kAshWhiteColor,
                       fontFamily: 'CM Sans Serif',
                       fontSize: 24.0,
-                      fontWeight: FontWeight.bold
-                    ),
+                      fontWeight: FontWeight.bold),
+                ),
+                TextSpan(text: '\n'),
+                TextSpan(
+                  text: '${getCurrentTime()}',
+                  style: TextStyle(
+                    color: kAshWhiteColor,
+                    fontFamily: 'CM Sans Serif',
+                    fontSize: 16.0,
+                    //fontWeight: FontWeight.bold
                   ),
-                  TextSpan(
-                    text: '\n'
-                  ),
-                  TextSpan(
-                    text: '${getCurrentTime()}',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'CM Sans Serif',
-                      fontSize: 16.0,
-                      //fontWeight: FontWeight.bold
-                    ),
-                  )
-                ]
-              ),
-
+                )
+              ]),
             ),
           ),
         ),
       ),
     );
   }
-}
-
-String getCurrentTime(){
-  DateTime now = DateTime.now();
-  String date = DateFormat('MMMM dd').format(now);
-  String time = getHourIn12(now);
-
-  return '$date at $time';
-}
-
-String getHourIn12(DateTime now){
-  if (now.hour <= 12) return '${now.hour}:${getMinute(now)} AM';
-  return '${now.hour-12}:${getMinute(now)} PM';
-}
-
-String getMinute(DateTime now){
-  if (now.minute < 10) return '0${now.minute}';
-  return now.minute.toString();
 }
