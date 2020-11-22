5668ee97d65cf38e6c996f27eda3a41bed3846dc
arrow <arrow@pop-os.localdomain>
2020-11-22 13:00:33 +0500

    Create chat message model

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,15 +1,17 @@
-class User {
+import 'dart:convert';
+
+class ChatUser {
   final String firstName;
   final String lastName;
   final String email;
 
-  User(this.firstName, this.lastName, this.email);
+  ChatUser(this.firstName, this.lastName, this.email);
 
   @override
   bool operator ==(Object o) {
     if (identical(this, o)) return true;
 
-    return o is User &&
+    return o is ChatUser &&
         o.firstName == firstName &&
         o.lastName == lastName &&
         o.email == email;
@@ -21,4 +23,27 @@ class User {
   @override
   String toString() =>
       'User(firstName: $firstName, lastName: $lastName, email: $email)';
+
+  Map<String, dynamic> toMap() {
+    return {
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+    };
+  }
+
+  factory ChatUser.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return ChatUser(
+      map['firstName'],
+      map['lastName'],
+      map['email'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ChatUser.fromJson(String source) =>
+      ChatUser.fromMap(json.decode(source));
 }
